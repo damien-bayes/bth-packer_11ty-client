7a85624792ead6756cb105b7f7402153559bc541
Damien Bayes <damien.bayes.db@gmail.com>
2020-03-13 15:40:43 +0600

    Fix of including partials

--- .eleventy.js
+++ .eleventy.js
@@ -9,11 +9,13 @@ module.exports = config => {
   //Watching for modificaions in style directory
   sass("./src/assets/styles/bayesian-aspectus.scss", "./dist/styles/bayesian-aspectus.css");
 
+  /*
   let nunjucksEnvironment = new Nunjucks.Environment(
     new Nunjucks.FileSystemLoader("_includes")
   );
 
   config.setLibrary("njk", nunjucksEnvironment);
+  */
 
   /*
   config.setBrowserSyncConfig({
