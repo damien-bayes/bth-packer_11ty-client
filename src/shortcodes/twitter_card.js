8e56a412c4d7f2ca8fd15daa1f713a02c980c9f1
Damien Bayes <damien.bayes.db@gmail.com>
2020-04-08 17:21:57 +0600

    Create twitter_card.js

---
+++ src/shortcodes/twitter_card.js
@@ -0,0 +1,26 @@
+const createTwitterCard = (site, title, description, image, creator) => {
+    let content = '';
+
+    if(image) {
+      content += `
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image" content="${image}"/>`;
+    } 
+    
+    content += `<meta name="twitter:card" content="summary"/>`;
+
+    content += `
+      <meta name="twitter:site" content="${site}"/>
+      <meta name="twitter:title" content="${title}"/>
+      <meta name="twitter:description" content="${description}"/>`;
+
+    if(creator) {
+      content += `<meta name="twitter:creator" content="${creator}"/>`;
+    }
+
+    return content;
+}
+
+module.exports = {
+  createTwitterCard
+};
