22c30485aa7de35cf26178e738701ff4302ebe37
Damien Bayes <damien.bayes.db@gmail.com>
2020-04-20 00:59:25 +0600

    Update global.js

--- src/_includes/_js/global.js
+++ src/_includes/_js/global.js
@@ -29,4 +29,14 @@ window.addEventListener("DOMContentLoaded", e => {
     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
     f.parentNode.insertBefore(j, f);
   })(window, document, "script", "dataLayer", "GTM-5WH62H7");
+
+  /* 
+   * This method of identifying anonymous users uses the browser's storage mechanism, which is not shared across devices. 
+   * Because we do not have a an email address or user ID to identify the user across devices and browsers, they could see Appcues multiple times. 
+   * People who use multiple devices or "incognito" browsers are pretty aware of this fact and usually don't mind seeing a duplicate 
+   * message or two.
+   *
+   * @see: https://docs.appcues.com/article/438-anonymous-users 
+   */
+  Appcues.anonymous();
 });
