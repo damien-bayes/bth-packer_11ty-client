8eff7728d1f73da6b248bffa53a3a5345c9c7f9d
Damien Bayes <damien.bayes.db@gmail.com>
2020-12-03 18:02:09 +0600

    Create .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+/**
+ * Prettier configuration
+ *
+ * @see: https://prettier.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+  useTabs: false,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+};
